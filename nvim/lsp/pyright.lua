96ed7bf85da2a6e1f423923e348aaa4bd23d444a
Jake edison Bersabe <jakeedison.bersabe@gcash.com>
2025-08-26 18:53:21 +0800

    feat: python lsp

---
+++ nvim/lsp/pyright.lua
@@ -0,0 +1,15 @@
+return {
+	-- Command and arguments to start the server.
+	cmd = { "pyright-langserver", "--stdio" },
+	filetypes = { 'python' },
+	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" },
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true
+			}
+		}
+	}
+}
